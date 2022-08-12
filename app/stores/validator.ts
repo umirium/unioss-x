bd5a6f571b0ac913718c3e0b784f7d9a3ca5e642
SHOTA KABURAKI <shota.kaburaki@gmail.com>
2022-08-12 21:05:53 +0900

    update: set validation messages

--- app/stores/validator.ts
+++ app/stores/validator.ts
@@ -9,30 +9,37 @@ export const contactPersonalInfoSchema = (
   t: TFunction
 ): yup.SchemaOf<ContactPersonalInfoType> =>
   yup.object({
-    yourName: yup.string().max(20).required(),
-    kana: yup.string().max(20).required(),
-    email: yup.string().max(255).email().required(),
+    yourName: yup.string().max(20, t("max20")).required(t("required")),
+    kana: yup.string().max(20, t("max20")).required(t("required")),
+    email: yup
+      .string()
+      .max(255, t("max255"))
+      .email(t("email"))
+      .required(t("required")),
     emailRetype: yup
       .string()
       .oneOf([yup.ref("email")])
-      .required(),
+      .required(t("required")),
     phoneNumber: yup
       .string()
-      .max(13)
-      .matches(/^[0-9\\-]*$/),
-    postalCode: yup.string().max(7),
-    prefecture: yup.string().max(2),
-    city: yup.string().max(50),
-    address1: yup.string().max(50),
-    address2: yup.string().max(50),
+      .max(13, t("max13"))
+      .matches(/^[0-9\\-]*$/, t("tel")),
+    postalCode: yup
+      .string()
+      .matches(/^[0-9]*$/, t("postalCode"))
+      .max(7, t("max7")),
+    prefecture: yup.string().max(2, t("max2")),
+    city: yup.string().max(50, t("max50")),
+    address1: yup.string().max(50, t("max50")),
+    address2: yup.string().max(50, t("max50")),
   });
 
 export const contactInquirySchema = (
   t: TFunction
 ): yup.SchemaOf<ContactInquiryType> =>
   yup.object({
-    category: yup.string().max(2).required(),
-    productName: yup.string().max(128),
-    orderCode: yup.string().max(128),
-    inquiry: yup.string().max(1000).required(),
+    category: yup.string().max(2, t("max2")).required(t("required")),
+    productName: yup.string().max(128, t("max128")),
+    orderCode: yup.string().max(128, t("max128")),
+    inquiry: yup.string().max(1000, t("max1000")).required(t("required")),
   });
