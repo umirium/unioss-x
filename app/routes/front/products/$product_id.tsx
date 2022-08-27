eb6896d56a31518de016afac9cafdcf0c91bca8c
SHOTA KABURAKI <shota.kaburaki@gmail.com>
2022-08-27 14:01:54 +0900

    update: create simple layout of product detail

--- app/routes/front/products/$product_id.tsx
+++ app/routes/front/products/$product_id.tsx
@@ -1,19 +1,62 @@
+import { Box, Button, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import type { definitions } from "~/types/tables";
+import { db } from "~/utils/db.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ product_id: params.product_id });
+  const products = await db
+    .from<definitions["products"]>("products")
+    .select("*")
+    .eq("product_id", params.product_id)
+    .single();
+
+  return products ? products.data : [];
 };
 
 export default function Product() {
   // const { handleChangeBreadcrumbs } = useBreadcrumbs();
-  const { product_id } = useLoaderData();
+  const product = useLoaderData();
 
   // set Breadcrumbs
   // useEffect(() => {
   //   handleChangeBreadcrumbs(product_id);
   // });
 
-  return <>product_id: {product_id}</>;
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid xs={12} sm={12} md={6}>
+          <img
+            src={product.image_url}
+            alt={product.product_name}
+            width="100%"
+          />
+        </Grid>
+        <Grid xs={12} sm={12} md={6}>
+          <Typography variant="h4">{product.product_name}</Typography>
+          <Box sx={{ mt: 3 }}>{product.description}</Box>
+          <Box sx={{ mt: 3 }}>
+            金額：
+            <Typography variant="h5" component="span">
+              {product.price.toLocaleString()}
+            </Typography>
+            円
+          </Box>
+          <Box sx={{ mt: 5, textAlign: "center" }}>
+            <Button
+              variant="contained"
+              color="warning"
+              size="large"
+              endIcon={<AddShoppingCartIcon />}
+            >
+              カートに入れる
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
